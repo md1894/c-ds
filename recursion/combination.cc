edf9e032a9603f2e4a27dbd45c90db567a94b081
kimaya <kimaya@localhost.localdomain>
2019-11-01 09:50:09 +0530

    combination by recursion

--- recursion/combination.cc
+++ recursion/combination.cc
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
-int comb(int n,int r){
-    
+int c(int n,int r){
+        if(n==r || r==0){
+            return 1;
+        }else{
+            return c(n-1,r)+c(n-1,r-1);
+        }
+}
+
+// find the combination by using pascal triangle
+
+int main(){
+    printf(" n = 6 , r = 3 == %d",c(12,5));
+    return 0;
 }
