ed4c1ca3e34f56fe6341453d5c1a3e6f18c11a5a
kimaya <kimaya@localhost.localdomain>
2019-11-06 16:19:52 +0530

    linear search

---
+++ array/lsearch.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int rlsearch(int* iparr,int left,int right,int key){
+    if(right < left){
+        return -1;
+    }
+    if(key == iparr[left]){
+        return left;
+    }
+    if(key == iparr[right]){
+        return right;
+    }
+    return rlsearch(iparr,left+1,right-1,key);
+}
+
+int ilsearch(int* iparr,int size,int key){
+    for(int i = 0 ; i < size ; i++){
+        if(key == iparr[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+    int arr[6] = {23,46,135,200,252,785};
+    printf("\n element returned by bsearch is %d",arr[rlsearch(arr,0,5,200)]);
+    printf("\n element returned by bsearch is %d",arr[ilsearch(arr,6,200)]);
+    return 0;
+}
