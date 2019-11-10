33c1c83bd1bde5400d94b1b81db125bcb6c95042
mehulkumar18021994 <you@example.com>
2019-11-11 00:35:42 +0530

    combinations of a string

---
+++ string/combi.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define MAX 100
+
+void comb(char* s,int k){
+    int i;
+    static int a[MAX]={0};
+    static char res[MAX];
+    if(s[k] == '\0'){
+        res[k] = '\0';
+        printf("%s\n",res);
+    }
+    else{
+        for(i=0;s[i]!='\0';i++){
+            if(a[i] == 0){
+                a[i] = 1;
+                res[k] = s[i];
+                comb(s,k+1);
+                a[i] = 0;
+            }
+        }
+    }
+}
+
+
+int main(){
+    char str[4] = {'a','b','c','\0'};
+    comb(str,0);
+    return 0;
+}
