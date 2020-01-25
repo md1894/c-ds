991cb3afaf15411c92d262742bc98f60d9b0587f
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-01-25 21:46:07 +0530

    operator overloading completed

--- oops/op_overloading/fraction.cpp
+++ oops/op_overloading/fraction.cpp
@@ -2,41 +2,94 @@
 using namespace std;
 
 
+/*
+     Note that for post increment operator we cant do nesting
+     with post increment operator --> example (i++)++ --> it gives compilation error
+     , if we overload post-increment operator , and do nesting then 
+     it will give unpredictable result
+*/
+
 class Fraction {
 
 public:
      Fraction(int numerator, int denominator):m_Numerator(numerator), m_Denominator(denominator) {
-         
+         simplify();
      }
      
      void set(int numerator, int denominator){
          m_Numerator = numerator;
-        m_Denominator = denominator;
+         m_Denominator = denominator;
+         simplify();
+     }
+
+     int gcd(int a, int b){
+          if(a % b == 0)
+               return b;
+          else
+               return gcd(b, a % b);
+     }
+
+     void simplify(){
+          int fact = gcd(m_Denominator, m_Numerator);
+          this->m_Numerator = this->m_Numerator / fact;
+          this->m_Denominator = this->m_Denominator / fact;
      }
      
-     void print(){
+     void print()const{
          cout<<m_Numerator<<" / "<<m_Denominator<<endl;
      }
      
      
-     int findGCD(int a, int b){
-         
+     Fraction operator+(const Fraction& other) const {
+         int n = (this->m_Numerator*other.m_Denominator) + (this->m_Denominator*other.m_Numerator);
+         int d = this->m_Denominator * other.m_Denominator;
+         return Fraction(n, d);
      }
-     
-     void simplify(){
-         
+
+     Fraction operator-(const Fraction& other) const {
+         int n = (this->m_Numerator*other.m_Denominator) - (this->m_Denominator*other.m_Numerator);
+         int d = this->m_Denominator * other.m_Denominator;
+         return Fraction(n, d);
      }
-     
-     Fraction operator+(const Fraction& other){
-         
+
+     Fraction & operator++(){
+          this->m_Numerator = this->m_Denominator + this->m_Numerator;
+          simplify();
+          return *this;
      }
-     Fraction operator-(const Fraction& other){
-         
-     }
+
+     // overloading post increment operator
+     Fraction operator++(int){
+          Fraction newFra(m_Numerator, m_Denominator);
+          this->m_Numerator = this->m_Denominator + this->m_Numerator;
+          simplify();
+          return newFra;
+     } 
      
 
 private:
      int m_Numerator;
      int m_Denominator;
 };
+
+
+int main(){
+     Fraction f(12,8);
+     f.print();
+     f.set(9,8);
+     f.print();
+
+     Fraction f1(2, 3);
+     Fraction f2(3, 2);
+     (f1+f2).print();
+     (f1-f2).print();
+
+     Fraction f3(2,3);
+     Fraction f4 = ++(++f3);
+     f3.print();
+     f4.print();
+     (f4++).print();
+     f4.print();
+     return 0;
+}
 
