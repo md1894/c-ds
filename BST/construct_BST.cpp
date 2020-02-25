253c25d4f1b2f52068119e4750e5a06b68900327
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-02-25 12:07:54 +0530

    BST CONT

--- BST/construct_BST.cpp
+++ BST/construct_BST.cpp
@@ -39,12 +39,19 @@ class BinaryTreeNode {
     }
 };
 
+BinaryTreeNode<int>* constructTree1(int *input, int start, int end){
+    if(start > end)
+        return NULL;
+    
+    int mid = (start + end)/2;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(input[mid]);
+    root->left = constructTree1(input, start, mid - 1);
+    root->right = constructTree1(input, mid + 1, end);
+    return root;
+}
+
 BinaryTreeNode<int>* constructTree(int *input, int n) {
-    /* Don't write main().
-     * Don't read input, it is passed as function argument.
-     * Return output and don't print it.
-     * Taking input and printing output is handled automatically.
-     */
+    return constructTree1(input, 0, n-1);
 
 }
 
--- BST/isBST.cpp
+++ BST/isBST.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -52,10 +53,80 @@ BinaryTreeNode<int>* takeInput() {
     }
     return root;
 }
+/*************************************************************************************************/
+// class pair1{
+//     public:
+//     bool isBst;
+//     int min;
+//     int max;
+// };
+
+// pair1 isBST1(BinaryTreeNode<int>* root){
+//     if(root == NULL){
+//         pair1 p;
+//         p.min = INT_MAX;
+//         p.max = INT_MIN;
+//         p.isBst = true;
+//         return p;
+//     }
+//     pair1 leftNode = isBST1(root->left);
+//     pair1 rightNode = isBST1(root->right);
+//     pair1 ans;
+//     ans.min = min(root->data, min(leftNode.min, rightNode.min));
+//     ans.max = max(root->data, max(leftNode.max, rightNode.max));
+//     ans.isBst = leftNode.max < root->data && rightNode.min > root->data && leftNode.isBst && rightNode.isBst;
+//     return ans;
+// }
+
+// bool isBST(BinaryTreeNode<int> *root){
+//     return isBST1(root).isBst;
+// }
+/*************************************************************************************************/
+// This is the worst solution
+
+// int minimum(BinaryTreeNode<int>* root){
+//     if(root == NULL){
+//         return INT_MAX;
+//     }
+
+//     return min(root->data, min(minimum(root->left), minimum(root->right)));
+// }
+
+// int maximum(BinaryTreeNode<int>* root){
+//     if(root == NULL){
+//         return INT_MIN;
+//     }
+//     return max(root->data, max(maximum(root->left), maximum(root->right)));
+// }
+
+// bool isBST(BinaryTreeNode<int> *root){
+//     if(root == NULL)
+//         return true;
+    
+//     int leftMax = maximum(root->left);
+//     int rightMin = minimum(root->right);
+//     bool isBst = root->data > leftMax && root->data < rightMin && isBST(root->left) & isBST(root->right);
+//     return isBst;
+// }
+/*************************************************************************************************/
+
+bool isBST1(BinaryTreeNode<int>* root, int Max = INT_MAX, int Min = INT_MIN){
+    if(root == NULL)
+        return true;
+
+    if(root->data < Min || root->data > Max)
+        return false;
+    
+    bool isLeftOk = isBST1(root->left, root->data - 1,Min);
+    bool isRightOk = isBST1(root->right, Max, root->data);
+    return isLeftOk && isRightOk;
+}
 
 bool isBST(BinaryTreeNode<int> *root){
-
+    return isBST1(root);
 }
+
+
 
 int main() {
     BinaryTreeNode<int>* root = takeInput();
