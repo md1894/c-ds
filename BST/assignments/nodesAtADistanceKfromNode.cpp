05fd143d9352c927dffab2cf9484fe48c003de89
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-03-06 11:34:28 +0530

    completed bst

--- BST/assignments/nodesAtADistanceKfromNode.cpp
+++ BST/assignments/nodesAtADistanceKfromNode.cpp
@@ -82,9 +82,70 @@ BinaryTreeNode<int>* takeInput() {
     return root;
 }
 
+void printNodeAtDepthK(BinaryTreeNode<int>* root, int depth){
+    if(root == NULL)
+        return;
+    if(depth == 0){
+        cout<<root->data<<endl;
+        return;
+    }else if(depth > 0){
+        printNodeAtDepthK(root->left, depth - 1);
+        printNodeAtDepthK(root->right, depth - 1);
+    }
+}
+
+int printN(BinaryTreeNode<int> *root, int node, int k){
+  // Base Case 1: If tree is empty, return -1 
+    if (root == NULL) return -1; 
+  
+    // If target is same as root.  Use the downward function 
+    // to print all nodes at distance k in subtree rooted with 
+    // target or root 
+    if (root->data == node) 
+    { 
+        printNodeAtDepthK(root, k); 
+        return 0; 
+    } 
+  
+    // Recur for left subtree 
+    int dl = printN(root->left, node, k); 
+  
+    // Check if target node was found in left subtree 
+    if (dl != -1) 
+    { 
+         // If root is at distance k from target, print root 
+         // Note that dl is Distance of root's left child from target 
+         if (dl + 1 == k) 
+            cout << root->data << endl; 
+  
+         // Else go to right subtree and print all k-dl-2 distant nodes 
+         // Note that the right child is 2 edges away from left child 
+         else
+            printNodeAtDepthK(root->right, k-dl-2); 
+  
+         // Add 1 to the distance and return value for parent calls 
+         return 1 + dl; 
+    } 
+  
+    // MIRROR OF ABOVE CODE FOR RIGHT SUBTREE 
+    // Note that we reach here only when node was not found in left subtree 
+    int dr = printN(root->right, node, k); 
+    if (dr != -1) 
+    { 
+         if (dr + 1 == k) 
+            cout << root->data << endl; 
+         else
+            printNodeAtDepthK(root->left, k-dr-2); 
+         return 1 + dr; 
+    } 
+  
+    // If target was neither present in left nor in right subtree 
+    return -1; 
+}
+
 void nodesAtDistanceK(BinaryTreeNode<int> *root, int node, int k) {
     // Write your code here
-
+    printN(root, node, k);
 }
 
 int main() {
