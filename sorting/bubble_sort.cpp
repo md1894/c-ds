9908fcc30e7cfb2944338f6d6c8395e2b571d534
kimaya <kimaya@localhost.localdomain>
2020-03-07 18:56:16 +0530

    bubblesort desc

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -10,9 +10,18 @@ void swap(int a[], int i, int j){
 }
 
 void bubble_sort(int a[], int n){
-    for(int i = 0; i < n - 1; i++){
-        for(int j = 0; j < n - 1 - i; j++){
-            if(a[j] > a[j+1]){
+    /*
+     * we have to preform one pass less because:
+     * when it comes to compare a[end-1] and a[end]
+     * then we will not goto index 'end', therefore we travell
+     * 'n-1' passes
+     */
+    for(int i = 0; i < n - 1; i++)
+    {  
+        for(int j = 0; j < n - 1 - i; j++)
+	{
+            if(a[j] > a[j+1])
+	    {
                 swap(a, j, j+1);
             }
         }
