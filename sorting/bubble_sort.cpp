e9ea7d281de8d72a2ebaeb3186a466d29dc11767
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-03-08 10:06:27 +0530

    details in bubblesort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -12,12 +12,13 @@ void swap(int a[], int i, int j){
 void bubble_sort(int a[], int n){
     /*
      * we have to preform one pass less because:
-     * when it comes to compare a[end-1] and a[end]
-     * then we will not goto index 'end', therefore we travell
-     * 'n-1' passes
+     * when we sort the second last element , we know that the last element is the only element
+     * and if there is only one element rest are in sorted position , then that element is also sorted
      */
     for(int i = 0; i < n - 1; i++)
-    {  
+    {  /*
+	* Here Comparions will reduced by one , every iteration of 'i' loop (Outer)
+    	*/
         for(int j = 0; j < n - 1 - i; j++)
 	{
             if(a[j] > a[j+1])
@@ -28,11 +29,42 @@ void bubble_sort(int a[], int n){
     }
 }
 
+void AdaptiveBubbleSort(int a[], int n){
+	int flag;
+	for(int i = 0; i < n - 1; i++){
+		flag = 0;
+		for(int j = 0; j < n - 1 - i; j++){
+			if(a[j] > a[j+1]){
+				swap(a,j,j+1);
+				flag = 1;
+			}
+		}
+		if(flag == 0)
+			break;
+	}
+}
+
 int main(){
     int a[] = {8,3,9,1,2}, n = 5;
-    bubble_sort(a,n);
+    AdaptiveBubbleSort(a,n);
     for(int i = 0; i < n; i++){
         cout<<a[i]<<" ";
     }
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
