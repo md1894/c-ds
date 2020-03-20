f061653d7e5f6c7408fbc663e98b0e64d069328b
kimaya <kimaya@localhost.localdomain>
2020-03-20 12:43:48 +0530

    trie search and insert added 1.0

--- tries-data_structure/trie.h
+++ tries-data_structure/trie.h
@@ -16,27 +16,44 @@ private:
   void insertWord(TrieNode* root, string word){
       //base case
       if(word.size() == 0){
-	this->root->isTerminal = true;
-	return;
+        this->root->isTerminal = true;
+        return;
       }
       
       // small calculation
       int index = word[0] - 'a';
       TrieNode* child;
       if(this->root->children[index] != NULL){
-	  child = this->root->children[index];
+        child = this->root->children[index];
       }else{
-	// create a new node
-	child = new TrieNode(word[0]);
-	this->root->children[index] = child;
+        // create a new node
+        child = new TrieNode(word[0]);
+        this->root->children[index] = child;
       }
       
       // recursive call;
       insertWord(child, word.substr(1));
   }
   
+  bool searchWord(TrieNode* root, string word){
+      if(word.size() == 0 && root->isTerminal == true){
+          return true;
+      }else{
+          int index = word[0] - 'a';
+          if(root->children[index]){
+            return searchWord(root->children[index], word.substr(1));  
+          }else{
+              return false;
+          }
+      }
+  }
+  
 public:
   void insertWord(string word){
     insertWord(this->root, word);
+  }
+  
+  bool searchWord(string word){
+      return searchWord(this->root, word);
   }
 };
--- tries-data_structure/trieUse.cpp
+++ tries-data_structure/trieUse.cpp
@@ -6,5 +6,12 @@ int main(){
   Trie t;
   t.insertWord("mehul");
   t.insertWord("meher");
+  bool b = t.searchWord("mehul");
+  if(b){
+      cout<<"Mehul Found"<<endl;
+  }
+  if(t.searchWord("jayashree")){
+      cout<<"Jaya Found"<<endl;
+  }
   return 0;
 }
